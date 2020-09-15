90b9014b5099e6b61bb5fa211f315254946b1f99
ritanpicado <ritanpicado@gmail.com>
2020-09-15 12:19:33 +0100

    fixed js

--- src/index.js
+++ src/index.js
@@ -181,8 +181,9 @@ function handleSubmit(event) {
 function searchLocation(position) {
   let apiKey = "98a75b44ef40d2758688b751e4f7d20b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(displayWeatherCondition);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentLocation(event) {
